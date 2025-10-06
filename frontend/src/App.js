8a6bdc741fcb641bd403901dbec29105c30fde21
BAMBA-FALL <khadimmbackefall1@gmail.com>
2025-10-06 12:11:10 +0200

    Configuration de Docker Compose pour le projet

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
 
-const API_URL = 'http://localhost:5000/api/tasks';
+const API_URL = 'http://16.16.63.64:5000/api/tasks';
 
 function App() {
   const [tasks, setTasks] = useState([]);
